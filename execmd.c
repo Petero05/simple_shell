2eb0eb3fd632cee1f282a74664404fd8c3a3b7bb
Petero05 <tuyapeter@gmail.com>
2023-05-14 05:12:52 -0700

    Execute command

---
+++ execmd.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ *@execve: Execmd command
+ *argv: argument command
+ *
+ * */
+
+
+void execmd(char **argv)
+{
+char *command = NULL;
+
+if (argv)
+{
+	/* get the command */
+command = argv[0];
+
+	/*execute the command with execve */
+if (execve(command, argv, NULL) == -1)
+{
+perror("Error:");
+};
+}
+}
