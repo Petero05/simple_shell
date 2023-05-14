4166e121e1da4b6efb9ebdbf8115d877d9394e0f
skd-alt <sdingani@yahoo.com>
2023-05-14 07:14:30 -0700

    Builtin Functions

--- shell.h
+++ shell.h
@@ -59,4 +59,14 @@ int _mycd(info_t *info);
 
 int _myhelp(info_t *info);
 
+int _myhistory(info_t *info);
+
+int unset_alias(info_t *info, char *str);
+
+int set_alias(info_t *info, char *str);
+
+int print_alias(list_t *node);
+
+int _myalias(info_t *info);
+
 #endif
