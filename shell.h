167d6555fa67ab01d06114cd781be8821cc050c4
Petero05 <tuyapeter@gmail.com>
2023-05-14 05:46:02 -0700

    Main Functions

--- shell.h
+++ shell.h
@@ -14,6 +14,40 @@
 #include <fcntl.h> /* for open files*/
 #include "macros.h" /* for msg help and prompt */
 
-void execmd(char **agrv);
 
+/**
+ * struct info- struct for the program's data
+ * @program_name: the name of the executable
+ * @input_line: pointer to the input read for _getline
+ * @command_name: pointer to the first command typed by the user
+ * @exec_counter: number of excecuted comands
+ * @file_descriptor: file descriptor to the input of commands
+ * @tokens: pointer to array of tokenized input
+ * @env: copy of the environ
+ * @alias_list: array of pointers with aliases.
+ */
+typedef struct info
+{
+	char *program_name;
+	char *input_line;
+	char *command_name;
+	int exec_counter;
+	int file_descriptor;
+	char **tokens;
+	char **env;
+	char **alias_list;
+} data_of_program;
+
+/* Inicialize the struct with the info of the program */
+void inicialize_data(data_of_program *data, int arc, char *argv[], char **env);
+
+/* Makes the infinite loop that shows the prompt*/
+void sisifo(char *prompt, data_of_program *data);
+
+/* Print the prompt in a new line */
+void handle_ctrl_c(int opr UNUSED);
+
+int main(int argc, char *argv[], char *env[]);
+
+void execmd(char **argv);
 #endif
