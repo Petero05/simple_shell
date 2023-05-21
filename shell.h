b20b69d8547b16329804a6bbdff0e43d0911844c
Petero05 <tuyapeter@gmail.com>
2023-05-21 08:32:52 -0700

    Updated Prototypes

--- shell.h
+++ shell.h
@@ -273,4 +273,32 @@ int replace_vars(info_t *);
 
 int replace_string(char **, char *);
 
+int interactive(info_t *);
+
+int is_delim(char, char *);
+
+int _isalpha(int);
+
+int _atoi(char *);
+
+list_t *add_node(list_t **, const char *, int);
+
+list_t *add_node_end(list_t **, const char *, int);
+
+size_t print_list_str(const list_t *);
+
+int delete_node_at_index(list_t **, unsigned int);
+
+void free_list(list_t **);
+
+size_t list_len(const list_t *);
+
+char **list_to_strings(list_t *);
+
+size_t print_list(const list_t *);
+
+list_t *node_starts_with(list_t *, char *, char);
+
+ssize_t get_node_index(list_t *, list_t *);
+
 #endif
