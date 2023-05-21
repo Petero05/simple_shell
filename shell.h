160ee5ce4ec7839bc40ef1c49c8cebb9f9b6548c
Petero05 <tuyapeter@gmail.com>
2023-05-21 04:30:07 -0700

    Updated Prototypes

--- shell.h
+++ shell.h
@@ -167,4 +167,16 @@ int _unsetenv(info_t *, char *);
 
 int _setenv(info_t *, char *, char *);
 
+/* history file --- history.c */
+
+char *get_history_file(info_t *info);
+
+int write_history(info_t *info);
+
+int read_history(info_t *info);
+
+int build_history_list(info_t *info, char *buf, int linecount);
+
+int renumber_history(info_t *info);
+
 #endif
