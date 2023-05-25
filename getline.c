e847b29cc5d9858bb9a61d10cf3f3df55ec88b83
skd-alt <sdingani@yahoo.com>
2023-05-24 18:30:59 -0700

    Update Functions

--- getline.c
+++ getline.c
@@ -97,9 +97,9 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 	ssize_t r = 0;
 	size_t len_p = 0;
 
-	if (!*len) 
+	if (!*len)
 	{
-	
+
 		free(*buf);
 		*buf = NULL;
 		r = getline(buf, &len_p, stdin);
@@ -107,7 +107,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 		{
 			if ((*buf)[r - 1] == '\n')
 			{
-				(*buf)[r - 1] = '\0'; 
+				(*buf)[r - 1] = '\0';
 				r--;
 			}
 			info->linecount_flag = 1;
@@ -131,7 +131,8 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 
 ssize_t get_input(info_t *info)
 {
-	static char *buf;       static size_t i, j, len;
+	static char *buf;
+	static size_t i, j, len;
 	ssize_t r = 0;
 	char **buf_p = &(info->arg), *p;
 
